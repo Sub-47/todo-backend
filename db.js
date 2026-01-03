2575796dcb91a8aa86988e0ab83e598479126537
Subham Chapagain <subhamchapagainop@gmail.com>
2026-01-03 14:58:15 +0545

    use render db

--- db.js
+++ db.js
@@ -4,12 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  host: 'db.lfoeumjnajvygplkdvqd.supabase.co',
-  port: 5432,
-  database: 'postgres',
-  user: 'postgres.lfoeumjnajvygplkdvqd',
-  password: process.env.DB_PASSWORD
+  }
 });
 
 module.exports = pool;
