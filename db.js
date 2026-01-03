51496f366b8b355f5cadb45e4544d8d6bf4a9b64
Subham Chapagain <subhamchapagainop@gmail.com>
2026-01-03 14:50:06 +0545

    force ipv4

--- db.js
+++ db.js
@@ -4,7 +4,12 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  host: 'db.lfoeumjnajvygplkdvqd.supabase.co',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres.lfoeumjnajvygplkdvqd',
+  password: process.env.DB_PASSWORD
 });
 
 module.exports = pool;
